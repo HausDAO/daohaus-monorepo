041982020a54d01300deb930462ee7f7ae0f2b93
Keating <keating.dev@protonmail.com>
2022-02-15 13:01:27 -0700

    Test

---
+++ apps/haus-price/src/handlers/bar.ts
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+export const handler = async (event: any, context: any) => {
+  try {
+    const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xf2051511b9b121394fa75b8f7d4e7424337af687&vs_currencies=usd`;
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+    console.log('Lambda run');
+    return 'success';
+  } catch {
+    throw new Error('We failed');
+  }
+};
