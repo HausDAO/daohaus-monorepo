e82b1c8a2f3d05e2bc6188d0690fa50d4a04fe53
skuhlmann <samkuhlmann@gmail.com>
2022-03-08 13:02:18 -0700

    lambda hack

--- apps/haus-price/src/handlers/bar.ts
+++ apps/haus-price/src/handlers/bar.ts
@@ -5,6 +5,7 @@ export const handler = async (event: any, context: any) => {
     const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xf2051511b9b121394fa75b8f7d4e7424337af687&vs_currencies=usd`;
     try {
       const res = await axios.get(url);
+
       return res.data;
     } catch (err) {
       console.error(err);
--- apps/haus-price/src/handlers/foo.ts
+++ apps/haus-price/src/handlers/foo.ts
@@ -1,6 +1,29 @@
+const axios = require('axios');
+
 export const handler = async (event: any, context: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ foo: 'bar' }),
-  };
+  try {
+    console.log('yolo');
+    const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xf2051511b9b121394fa75b8f7d4e7424337af687&vs_currencies=usd`;
+    const res = await axios.get(url);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(res.data),
+    };
+  } catch {
+    // throw new Error('We failed');
+
+    return {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ error: 'bar' }),
+    };
+  }
 };
