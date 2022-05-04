0af429c8a0784ddabff592329417d8cd10ab1ac5
Alex Keating <keating.dev@protonmail.com>
2022-05-04 15:43:50 -0400

    Changes

--- libs/dao-data/jest.config.js
+++ libs/dao-data/jest.config.js
@@ -1,14 +1,19 @@
 module.exports = {
   displayName: 'data',
+  testEnvironment: 'node',
   preset: '../../jest.preset.js',
+  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true,
     },
   },
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
+
+  transformIgnorePatterns: ['node_modules/(?!(@self.*\\.[tj]s)$)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/dao-data',
 };
